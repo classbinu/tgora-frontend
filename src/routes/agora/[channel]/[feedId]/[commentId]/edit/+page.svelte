d7ec68519dab8961f9fe6f7355eae94522837653
Classbinu <classbinu@gmail.com>
2023-09-24 23:15:56 +0900

    댓글 수정시 필수 입력

--- src/routes/agora/[channel]/[feedId]/[commentId]/edit/+page.svelte
+++ src/routes/agora/[channel]/[feedId]/[commentId]/edit/+page.svelte
@@ -90,6 +90,7 @@
 					class="textarea textarea-bordered"
 					placeholder="보이지 않아도 모두 같은 동료 교사입니다. 상대방을 존중해 주세요."
 					rows="3"
+					required
 				/>
 			</div>
 			<button class="btn btn-success mt-5 w-full">수정</button>
