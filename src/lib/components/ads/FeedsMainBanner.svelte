2c97849783fc1ead4385687f210a6d3715753095
Classbinu <classbinu@gmail.com>
2023-10-03 21:58:33 +0900

    배너광고 테스트

--- src/lib/components/ads/FeedsMainBanner.svelte
+++ src/lib/components/ads/FeedsMainBanner.svelte
@@ -1,5 +1,7 @@
 <div class="p-1">
 	<div class="lg:w-1/2 mx-auto">
-		<img src="/kftu_banner.png" alt="교사노동조합연맹" class="card" />
+		<a href="https://www.kftu.net/" target="_blank">
+			<img src="/kftu_banner.png" alt="교사노동조합연맹" class="card" />
+		</a>
 	</div>
 </div>
