8d989d761ce6852046594125155e1a34f8d2fe03
Classbinu <classbinu@gmail.com>
2023-10-03 22:07:28 +0900

    피드 배너 해상도 복구

--- src/lib/components/ads/FeedsMainBanner.svelte
+++ src/lib/components/ads/FeedsMainBanner.svelte
@@ -1,7 +1,7 @@
 <div class="p-1">
 	<div class="lg:w-1/2 mx-auto">
 		<a href="https://www.kftu.net/" target="_blank">
-			<img src="/kftu_banner_1000.png" alt="교사노동조합연맹" class="card" />
+			<img src="/kftu_banner.png" alt="교사노동조합연맹" class="card" />
 		</a>
 	</div>
 </div>
