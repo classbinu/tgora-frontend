66af31a5dd43a4996204c61f13ed61382dcb7d3e
Classbinu <classbinu@gmail.com>
2023-10-03 22:30:48 +0900

    유출추적 안내 경량화

--- src/lib/components/FeedSecretWarning.svelte
+++ src/lib/components/FeedSecretWarning.svelte
@@ -1,6 +1,6 @@
 <div class="p-1 w-full lg:w-1/2 mx-auto">
 	<p class="text-error font-bold text-sm">피드를 캡처해서 외부로 유출하지 말아주세요 🙅</p>
 	<p class="text-sm text-gray-400">
-		피드는 조회 회원이 기록되며 UUID, IPv4 워터마크 등 보안 코드가 작동 중입니다 🔐
+		Logs, UUID/IPv4 Watermark 등 유출 추적 보안코드 실행 중 🔐
 	</p>
 </div>
