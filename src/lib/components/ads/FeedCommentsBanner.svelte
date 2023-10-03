607f4d95fc8158305f9e49133e6107c2d6c94c77
Classbinu <classbinu@gmail.com>
2023-10-04 07:16:09 +0900

    핸디북 배너 삭제

--- src/lib/components/ads/FeedCommentsBanner.svelte
+++ src/lib/components/ads/FeedCommentsBanner.svelte
@@ -1,5 +1,5 @@
 <div class="mx-auto">
-	<a href="https://indiepub.kr/product/초등학생이-좋아하는-글쓰기-소재-365-핸디북/596/category/24/display/1/" target="_blank">
-		<img src="/365_banner.png" alt="초등학생이 좋아하는 글쓰기 소재 365" class="card" />
+	<a href="https://www.kftu.net/" target="_blank">
+		<img src="/kftu_banner.png" alt="초등학생이 좋아하는 글쓰기 소재 365" class="card" />
 	</a>
 </div>
