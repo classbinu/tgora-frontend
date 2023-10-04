f2dff51d89a7f1e9fdff2f166d96729d4da4ccb2
Classbinu <classbinu@gmail.com>
2023-10-04 23:20:05 +0900

    maganer 표시 업그레이드

--- src/routes/issues/closed/+page.svelte
+++ src/routes/issues/closed/+page.svelte
@@ -305,7 +305,7 @@
 	<div class="text-center mt-20">
 		<a href="/" class="link link-success">진행 중인 이슈 보기</a>
 	</div>
-	<p class="text-center text-gray-400 mt-10">
+	<p class="text-center text-gray-400 mt-10 text-sm">
 		Managed by <span class="font-bold">익명의 선생님</span>
 	</p>
 </main>
