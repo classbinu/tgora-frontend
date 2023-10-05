0591fd01316589a7c4c79e3ba5929fa6fa320bea
Classbinu <classbinu@gmail.com>
2023-10-05 13:50:43 +0900

    초대장 광고 문구 변경

--- src/lib/components/ads/FeedCommentsBanner.svelte
+++ src/lib/components/ads/FeedCommentsBanner.svelte
@@ -1,5 +1,5 @@
 <a href="/mypage/invite">
 	<div class="btn md:btn-lg h-20 bg-yellow-300 hover:bg-yellow-200 flex justify-center font-bold">
-		<span>3초 만에 동학년 선생님에게 T-아고라 초대장 보내기 📮</span>
+		<span>3초 만에 주변 선생님에게 T-아고라 초대장 보내기 📮</span>
 	</div>
 </a>
